087d9ea4899023db850ffd681cabe80e8aecea3e
HammadM48 <hammadmahmood484@gmail.com>
2025-11-25 11:34:36 -0500

    Added Route to new Item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget{
+class GroceryList extends StatefulWidget{
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (ctx)=> const NewItem())
+    );
+  }
   @override 
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context)  {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Your Groceries")
+        title: const Text("Your Groceries"),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add_box))],
       ),
       body:ListView.builder(
         itemCount:groceryItems.length,
