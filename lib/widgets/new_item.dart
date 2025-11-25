81d977292198203d54b49c911ac064c268d23d90
HammadM48 <hammadmahmood484@gmail.com>
2025-11-25 12:04:39 -0500

    Added new row with buttons

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -54,6 +54,13 @@ class _NewItemState extends State<NewItem>{
                       ,)
                   ], onChanged: (value){}),
                 )
+              ],),
+              const SizedBox(height:12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                TextButton(onPressed: (){}, child: const Text("Reset")),
+                ElevatedButton(onPressed: (){}, child: Text("Add Item"))
               ],)
             ],
           ),
@@ -62,3 +69,5 @@ class _NewItemState extends State<NewItem>{
     );
   }
 }
+
+//Added new row with buttonsAdded new row with buttons
